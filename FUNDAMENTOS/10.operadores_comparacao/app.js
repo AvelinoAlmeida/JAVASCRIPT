df2df07111b0b216166f888b5d6fa4ca71390e5e
Avelino Almeida <avlalmeida@hotmail.com>
2022-01-15 15:46:09 +0000

    Create app.js

---
+++ FUNDAMENTOS/10.operadores_comparacao/app.js
@@ -0,0 +1,12 @@
+// OPERADORES DE COMPARAÇÃO
+
+/*
+Já conhecemos operadores de comparação de matemática
+Maior que, menor que, maior ou igaul a, etc.
+
+Vamos apreder a comparar valores e perceber o seu resultado.
+*/
+
+// IMPORTANTE:
+
+console.log(10 > 20);
