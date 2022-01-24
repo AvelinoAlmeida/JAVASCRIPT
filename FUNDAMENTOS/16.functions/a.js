d118ed27f73d142d3996f6f6d5d8869b2e403007
Avelino Almeida <63342959+AvelinoAlmeida@users.noreply.github.com>
2022-01-24 21:23:51 +0000

    Modulo 16.1

---
+++ FUNDAMENTOS/16.functions/a.js
@@ -0,0 +1,8 @@
+// FUNCAO SEM PARAMETROS
+console.clear();
+
+function apresentar_mensagem(){
+    console.log('ola Mundo')
+}
+
+apresentar_mensagem();
---
+++ FUNDAMENTOS/16.functions/app.js
@@ -0,0 +1,16 @@
+// FUNCÕES
+
+/*3Por vezes necessitamos executar semelhantes em varias locaizações da nossa aplicação.
+Nao se trata de criar um ciclo para calculos, mas sim uma forma de excetuar um determinado calcuo sempre que necessario.
+
+As funções sao uma das principais componentes na estrutura de programação do Javascript 
+*/
+
+function nome_da_funcao(argumento) {
+    //codigo da função
+}
+
+// para executar uma funcao, "chamamos" o nome dela e passamos os paramentros.
+
+//Uma funcao pode ser chamada antes de estar definifa ou depois de estar definida.
+
